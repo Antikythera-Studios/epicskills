b3fc42e4d14dfad3d17bebf31b6cff96c383de5d
Ellet <echo.ellet@gmail.com>
2025-11-03 16:38:09 +0300

    chore: play click sound when pressing the equip/unequip button via controller

--- src/main/java/com/yesman/epicskills/compat/controlify/ControlifyCompat.java
+++ src/main/java/com/yesman/epicskills/compat/controlify/ControlifyCompat.java
@@ -318,6 +318,7 @@ public class ControlifyCompat implements ControlifyEntrypoint {
         protected void handleButtons(ControllerEntity controller) {
             if (ACTION.on(controller).guiPressed().get()) {
                 screen.getActionButton().onPress();
+                playClackSound();
             }
             super.handleButtons(controller);
         }
