c96d7a6bde506d2e928e4256a3cc3faaeb1a6305
Yesssssman <yesman1058@gmail.com>
2025-11-07 20:29:57 +0900

    20.2.3
    Adopted KeyConflictContext

--- src/main/java/com/yesman/epicskills/client/input/EpicSkillsKeyMappings.java
+++ src/main/java/com/yesman/epicskills/client/input/EpicSkillsKeyMappings.java
@@ -6,12 +6,13 @@ import com.yesman.epicskills.EpicSkills;
 import net.minecraft.client.KeyMapping;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
+import net.minecraftforge.client.settings.KeyConflictContext;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = EpicSkills.MODID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class EpicSkillsKeyMappings {
-	public static final KeyMapping OPEN_SKILL_TREE = new KeyMapping(EpicSkills.format("key.%s.open_skill_tree"), InputConstants.KEY_N, EpicSkills.format("key.%s.gui"));
+	public static final KeyMapping OPEN_SKILL_TREE = new KeyMapping(EpicSkills.format("key.%s.open_skill_tree"), KeyConflictContext.IN_GAME, InputConstants.Type.KEYSYM, InputConstants.KEY_N, EpicSkills.format("key.%s.gui"));
 	
 	@SubscribeEvent
 	public static void registerKeys(RegisterKeyMappingsEvent event) {
