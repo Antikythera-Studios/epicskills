297d7188a5ea72e9f8461abddbf1989b91c9f4d4
Yesssssman <yesman1058@gmail.com>
2025-12-18 05:18:41 +0900

    Fixed dedicated server crash

--- src/main/java/com/yesman/epicskills/client/input/EpicSkillsKeyMappingHandler.java
+++ src/main/java/com/yesman/epicskills/client/input/EpicSkillsKeyMappingHandler.java
@@ -1,16 +1,19 @@
 package com.yesman.epicskills.client.input;
 
+import org.jetbrains.annotations.NotNull;
+
 import com.yesman.epicskills.EpicSkills;
 import com.yesman.epicskills.client.gui.screen.SkillTreeScreen;
+
 import net.minecraft.client.Minecraft;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import org.jetbrains.annotations.NotNull;
 import yesman.epicfight.client.ClientEngine;
 import yesman.epicfight.client.world.capabilites.entitypatch.player.LocalPlayerPatch;
 
-@Mod.EventBusSubscriber(modid = EpicSkills.MODID)
+@Mod.EventBusSubscriber(modid = EpicSkills.MODID, value = Dist.CLIENT)
 public class EpicSkillsKeyMappingHandler {
     @SubscribeEvent
     public static void onClientTick(TickEvent.ClientTickEvent event) {
